9c97fc261e3795d66afbf86d65c4267a5067dc74
akashlitoriya <96015948+akashlitoriya@users.noreply.github.com>
2023-11-16 22:59:37 +0530

    Dashboard page added

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Outlet } from 'react-router-dom';
+import Sidebar from '../components/core/Dashboardcomp/Sidebar';
+
+
+const Dashboard = () => {
+    const{loading: profileLoading} = useSelector((state) => state.profile);
+    const{loading: authLoading} = useSelector((state) => state.auth);
+
+    if(profileLoading || authLoading){
+        return (
+            <div className='font-inter text-3xl text-richblack-5 mt-10'>Loading...</div>
+        )
+    }
+
+  return (
+    <div className='w-full flex'>
+      <div className='hidden md:static min-h-[calc(100vh-4rem)] bg-richblack-800'>
+        <Sidebar />
+      </div>
+      <div className=' min-h-[calc(100vh-4rem)] w-full'>
+        <Outlet />
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
