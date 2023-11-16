ed0069f6fd03da0aedeb0fb0f756a22a8bc8d3cc
akashlitoriya <96015948+akashlitoriya@users.noreply.github.com>
2023-11-16 11:27:18 +0530

    Contact us page added

--- src/components/core/ContactUs/ContactForm.jsx
+++ src/components/core/ContactUs/ContactForm.jsx
@@ -4,29 +4,29 @@ import countryCodes from '../../../data/countrycode.json'
 const ContactForm = () => {
     const {register} = useForm();
   return (
-    <div className='col-span-2 border-[1px] border-richblack-700 rounded-xl flex flex-col lg:p-10 lg:gap-10'>
+    <div className='col-span-1 md:col-span-2 border-[1px] border-richblack-700 rounded-xl flex flex-col p-3 lg:p-10 gap-5 lg:gap-10'>
         <div className='flex flex-col gap-3'>
-            <p className='font-inter font-semibold text-3xl text-richblack-5'>Got a Idea? We’ve got the skills.<br></br> Let’s team up</p>
-            <p className='font-inter text-richblack-200 text-base'>Tall us more about yourself and what you’re got in mind.</p>
+            <p className='font-inter font-semibold text-xl md:text-3xl text-richblack-5'>Got a Idea? We’ve got the skills.<br></br> Let’s team up</p>
+            <p className='font-inter text-richblack-200 text-sm md:text-base'>Tall us more about yourself and what you’re got in mind.</p>
         </div>
         <form>
             <div className='flex flex-col gap-5'>
-                <div className='grid grid-flow-col grid-cols-2 gap-x-3'>
+                <div className='grid grid-flow-col grid-cols-1 grid-rows-2 md:grid-rows-1 gap-3 md:grid-cols-2 gap-x-3'>
                     <div className='flex flex-col gap-1'>
-                        <label htmlFor='firstName' className='text-richblack-25 font-inter text-base'>First Name</label>
+                        <label htmlFor='firstName' className='text-richblack-25 font-inter text-sm md:text-base'>First Name</label>
                         <input type='text' className='p-3 bg-richblack-800 border-b-[2px] border-richblack-700 rounded-lg' placeholder='John' {...register('firstName')}/>
                     </div>
                     <div className='flex flex-col gap-1'>
-                        <label htmlFor='lastName' className='text-richblack-25 font-inter text-base'>Last Name</label>
+                        <label htmlFor='lastName' className='text-richblack-25 font-inter text-sm md:text-base'>Last Name</label>
                         <input type='text' className='p-3 bg-richblack-800 border-b-[2px] border-richblack-700 rounded-lg' placeholder='Cena' {...register('lastName')}/>
                     </div>
                 </div>
                 <div  className='flex flex-col gap-1'>
-                        <label htmlFor='email' className='text-richblack-25 font-inter text-base'>Email</label>
+                        <label htmlFor='email' className='text-richblack-25 font-inter text-sm md:text-base'>Email</label>
                         <input type='email' className='w-full p-3 bg-richblack-800 border-b-[2px] border-richblack-700 rounded-lg' placeholder='Enter your email here' {...register('email')}/>
                 </div>
                 <div className='flex flex-col gap-2'>
-                    <label htmlFor='contactNumber' className='text-richblack-25 font-inter text-base'>Phone Number</label>
+                    <label htmlFor='contactNumber' className='text-richblack-25 font-inter text-sm md:text-base'>Phone Number</label>
                     <div className='flex flex-row gap-3'>
                         <select {...register('countryCode')} className='p-3 bg-richblack-800 w-[80px] text-richblack-100 rounded-lg text-base'>
                             {
@@ -39,7 +39,7 @@ const ContactForm = () => {
                     </div>
                 </div>
                 <div  className='flex flex-col gap-1'>
-                        <label htmlFor='message' className='text-richblack-25 font-inter text-base'>Message</label>
+                        <label htmlFor='message' className='text-richblack-25 font-inter text-sm md:text-base'>Message</label>
                         <textarea rows={5} type='message' className='w-full p-3 bg-richblack-800 border-b-[2px] border-richblack-700 rounded-lg' placeholder='Type your message here' {...register('message')}/>
                 </div>
                 <button className='w-full bg-yellow-50 text-richblack-900 font-inter font-semibold p-3 rounded-lg transition-all duration-200 hover:scale-95'>Submit</button>
--- src/components/core/ContactUs/ContactInfo.jsx
+++ src/components/core/ContactUs/ContactInfo.jsx
@@ -2,36 +2,36 @@ import React from 'react'
 import {HiChatBubbleLeftRight,HiBuildingOffice2,HiPhone} from 'react-icons/hi2'
 const ContactInfo = () => {
   return (
-    <div className='bg-richblack-800 lg:p-8 rounded-xl h-fit'>
+    <div className='bg-richblack-800 p-6 md:p-8 rounded-xl h-fit'>
       <div className='flex flex-col gap-3 '>
         <div className='flex gap-2'>
             <div>
-                <HiChatBubbleLeftRight className='text-richblack-5 text-2xl'/>
+                <HiChatBubbleLeftRight className='text-richblack-5 text-base md:text-2xl'/>
             </div>
             <div className='flex flex-col gap-1'>
-                <p className='text-richblack-5 font-inter font-semibold text-lg'>Chat with us</p>
-                <p className='text-richblack-200 font-inter text-sm'>Our friendly team is here to help.</p>
-                <p className='text-richblack-200 font-inter text-sm'>@mail address</p>
+                <p className='text-richblack-5 font-inter font-semibold text-base md:text-lg'>Chat with us</p>
+                <p className='text-richblack-200 font-inter text-xs md:text-sm'>Our friendly team is here to help.</p>
+                <p className='text-richblack-200 font-inter text-xs md:text-sm'>@mail address</p>
             </div>
         </div>
         <div className='flex gap-2'>
             <div>
-                <HiBuildingOffice2 className='text-richblack-5 text-2xl'/>
+                <HiBuildingOffice2 className='text-richblack-5 text-base md:text-2xl'/>
             </div>
             <div className='flex flex-col gap-1'>
-                <p className='text-richblack-5 font-inter font-semibold text-lg'>Visit us</p>
-                <p className='text-richblack-200 font-inter text-sm'>Come and say hello at our office HQ.</p>
-                <p className='text-richblack-200 font-inter text-sm'>Here is the location/ address</p>
+                <p className='text-richblack-5 font-inter font-semibold text-base md:text-lg'>Visit us</p>
+                <p className='text-richblack-200 font-inter text-xs md:text-sm'>Come and say hello at our office HQ.</p>
+                <p className='text-richblack-200 font-inter text-xs md:text-sm'>Here is the location/ address</p>
             </div>
         </div>
         <div className='flex gap-2'>
             <div>
-                <HiPhone className='text-richblack-5 text-2xl'/>
+                <HiPhone className='text-richblack-5 text-base md:text-2xl'/>
             </div>
             <div className='flex flex-col gap-1'>
-                <p className='text-richblack-5 font-inter font-semibold text-lg'>Call us</p>
-                <p className='text-richblack-200 font-inter text-sm'>Mon - Fri From 8am to 5pm</p>
-                <p className='text-richblack-200 font-inter text-sm'>+123 456 7890</p>
+                <p className='text-richblack-5 font-inter font-semibold text-base md:text-lg'>Call us</p>
+                <p className='text-richblack-200 font-inter text-xs md:text-sm'>Mon - Fri From 8am to 5pm</p>
+                <p className='text-richblack-200 font-inter text-xs md:text-sm'>+123 456 7890</p>
             </div>
         </div>
       </div>
