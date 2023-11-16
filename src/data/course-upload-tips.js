976c3413ac3a957f8ebdfb05adf66b89da06f3b5
akashlitoriya <96015948+akashlitoriya@users.noreply.github.com>
2023-11-16 23:08:36 +0530

    Course upload tips data for add course is added

---
+++ src/data/course-upload-tips.js
@@ -0,0 +1,30 @@
+export const courseUploadTips = [
+    {
+        id:1,
+        text:"Set the Course Price option or make it free."
+    },
+    {
+        id:2,
+        text:"Standard size for the course thumbnail is 1024x576."
+    },
+    {
+        id:3,
+        text:"Course Builder is where you create & organize a course."
+    },
+    {
+        id:4,
+        text: "Add Topics in the Course Builder section to create lessons, quizzes, and assignments."
+    },
+    {
+        id:5,
+        text: "Information from the Additional Data section shows up on the course single page."
+    },
+    {
+        id:6,
+        text: "Make Announcements to notify any important"
+    },
+    {
+        id:7,
+        text: "Notes to all enrolled students at once."
+    }
+]
