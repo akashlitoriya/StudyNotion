e8cf6cfd3797594c181382fa42d9d474e6839b9c
akashlitoriya <96015948+akashlitoriya@users.noreply.github.com>
2023-11-16 16:28:25 +0530

    improved padding of contact-us page for mobile devices

--- src/components/core/ContactUs/index.jsx
+++ src/components/core/ContactUs/index.jsx
@@ -4,7 +4,7 @@ import ContactInfo from "./ContactInfo"
 export default function ContactUs(){
     return (
         <div>
-            <div className="w-11/12 mt-14 p-4 md:p-0 max-w-maxContent mx-auto flex flex-col md:grid md:grid-flow-col grid-cols-1 grid-rows-2 md:grid-rows-1 md:grid-cols-3 gap-10 lg:py-10">
+            <div className="w-11/12 mt-14 pt-4 md:p-0 max-w-maxContent mx-auto flex flex-col md:grid md:grid-flow-col grid-cols-1 grid-rows-2 md:grid-rows-1 md:grid-cols-3 gap-10 lg:py-10">
                 <ContactInfo />
                 <ContactForm />
             </div>
