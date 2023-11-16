f8e322ce06c9c3a4afe2932e1306823cc048802e
akashlitoriya <96015948+akashlitoriya@users.noreply.github.com>
2023-11-16 23:09:09 +0530

    Course slice is created and added in the store

---
+++ src/slices/courseSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    step: 1,
+    course: null,
+    editCourse: false,
+    paymentLoading:false
+}
+const courseSlice = createSlice({
+    name:"course",
+    initialState,
+    reducers: {
+        setStep: (state, action) => {
+            state.step = action.payload
+        },
+        setCourse: (state, action) => {
+            state.course = action.payload
+        },
+        setEditCourse: (state, action) => {
+            state.editCourse = action.payload
+        },
+        setPaymentLoading: (state, action) => {
+            state.paymentLoading = action.payload
+        },
+        resetCourse: (state) => {
+            state.step = 1
+            state.course = null
+            state.editCourse = false
+        },
+    }
+})
+
+export const{setCourse, setEditCourse, setPaymentLoading, resetCourse, setStep} = courseSlice.actions;
+export default courseSlice.reducer;
