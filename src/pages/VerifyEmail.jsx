af3521cc649d215b3798a23df95eb243da111ecb
akashlitoriya <96015948+akashlitoriya@users.noreply.github.com>
2023-11-17 18:41:36 +0530

    Responsiveness added into the verify-email page

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -39,26 +39,26 @@ const VerifyEmail = () => {
         :
         (
             <div className='max-w-[588px] flex flex-col gap-5'>
-                <h1 className='text-3xl font-bold font-inter text-richblack-25'>Verify Email</h1>
-                <p className='text-lg text-richblack-300'>A verification code has been sent to you. Enter the code below</p>
+                <h1 className='text-xl md:text-3xl font-bold font-inter text-richblack-25'>Verify Email</h1>
+                <p className='text-sm md:text-lg text-richblack-300'>A verification code has been sent to you. Enter the code below</p>
                 <form onSubmit={handleOnSubmit} className='flex flex-col justify-center items-center gap-7'>
                     <OTPInput
                         value={otp}
                         onChange={setOTP}
                         numInputs={6}
                         renderSeparator={<span className='text-richblack-300 font-bold'>-</span>}
-                        renderInput={(props) => <input {...props} className='mx-3 !w-[57px] h-[58px] bg-richblack-800 rounded-lg text-white text-xl'/>}
+                        renderInput={(props) => <input {...props} className='mx-1 md:mx-3 !w-[30px] h-[40px] md:!w-[57px] md:h-[58px] bg-richblack-800 rounded-lg text-white text-xl'/>}
                     />
-                    <button type='submit' className='bg-yellow-50 font-semibold text-richblack-700 p-3 w-full transition-all duration-200 hover:bg-yellow-300 rounded-lg'>Verify Email</button>
+                    <button type='submit' className='bg-yellow-50 font-semibold text-richblack-700 p-3 text-sm md:text-base w-full transition-all duration-200 hover:bg-yellow-300 rounded-lg'>Verify Email</button>
                 </form>
                 <div className='flex justify-between items-center'>
                     <Link to='/login'>
-                        <div className='text-richblack-50 flex gap-1 items-center hover:text-richblack-200 transition-all duration-200'>
+                        <div className='text-richblack-50 flex gap-1 text-sm md:text-base items-center hover:text-richblack-200 transition-all duration-200'>
                             <BsArrowLeft />
                             Back to Login
                         </div>
                     </Link>
-                    <div className='text-blue-100 flex gap-1 items-center hover:text-blue-200 transition-all duration-200 cursor-pointer' onClick={() => dispatch(sendOtp(signupData.email, navigate))}>
+                    <div className='text-blue-100 flex gap-1 text-sm md:text-base items-center hover:text-blue-200 transition-all duration-200 cursor-pointer' onClick={() => dispatch(sendOtp(signupData.email, navigate))}>
                         <PiClockCounterClockwiseBold />
                         <span>Resend it</span>
                     </div>
